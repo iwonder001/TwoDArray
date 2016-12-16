fab721854e3a802d49bad3449760b78ab4a94bca
Kristen <kcarethers@gmail.com>
2016-12-16 10:56:30 -0500

    Initial commit and created TwoDArray class

---
+++ src/TwoDArray.java
@@ -0,0 +1,35 @@
+public class TwoDArray {
+
+	private final int ROWS_SIZE = 2;
+	private final int COLS_SIZE = 3;
+
+	int[][] numberArray = new int[ROWS_SIZE][COLS_SIZE];
+
+	public TwoDArray(int[][] temp) {
+		for (int row = 0; row < ROWS_SIZE; row++) {
+			for (int col = 0; col < COLS_SIZE; col++) {
+				this.numberArray[row][col] = temp[row][col];
+			}
+		}
+
+	}
+
+	public TwoDArray() {
+		int[][] numberArray = { { 1, 1, 1 }, { 1, 1, 1 } };
+	}
+
+	public int getTotal() {
+		int total = 0;
+		for (int r = 0; r < ROWS_SIZE; r++) {
+			for (int c = 0; c < COLS_SIZE; c++) {
+				total += numberArray[r][c];
+
+			}
+		}
+		return total;
+	}
+	public double getAverage(){
+		//casting integer to double
+		return (double)getTotal()/(ROWS_SIZE * COLS_SIZE);
+	}
+}
