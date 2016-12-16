8a625b3310fb4643f2b6dc17b77e379f2f72acbb
Kristen <kcarethers@gmail.com>
2016-12-16 11:15:00 -0500

    Finished all the methods in the TwoDArray class

--- src/TwoDArray.java
+++ src/TwoDArray.java
@@ -28,16 +28,48 @@ public class TwoDArray {
 		}
 		return total;
 	}
-	public double getAverage(){
-		//casting integer to double
-		return (double)getTotal()/(ROWS_SIZE * COLS_SIZE);
+
+	public double getAverage() {
+		// casting integer to double
+		return (double) getTotal() / (ROWS_SIZE * COLS_SIZE);
 	}
-	public double getRowTotal(int r){
+
+	public double getRowTotal(int r) {
 		int rowTotal = 0;
-		for(int c = 0; c < COLS_SIZE; c++){
-			rowTotal += numberArray [r][c];
-			
+		for (int c = 0; c < COLS_SIZE; c++) {
+			rowTotal += numberArray[r][c];
+
 		}
 		return rowTotal;
+	}
+
+	public double getColTotal(int c) {
+		int colTotal = 0;
+		for (int r = 0; r < COLS_SIZE; r++) {
+			colTotal += numberArray[r][c];
+
+		}
+		return colTotal;
+	}
+
+	public int getHightestInRow(int row) {
+		int maxValue = numberArray[row][0];
+		for (int c = 1; c < COLS_SIZE; c++) {
+			if (maxValue < numberArray[row][c]) {
+				maxValue = numberArray[row][c];
+			}// end if
+		}// end for
+		return maxValue;
+	}
+
+	public int getLowestInRow(int row) {
+		int minValue = numberArray[row][0];
+		for (int c = 1; c < COLS_SIZE; c++) {
+			if (minValue > numberArray[row][c]) {
+				minValue = numberArray[row][c];
+			}// end if
+		}// end for
+		return minValue;
+
 	}
 }
